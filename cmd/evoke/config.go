4623d2f80aae46ea6044bfe1e14107d7a5fa8f76
Bitlatte <cobymckinney2025@gmail.com>
2025-07-06 01:24:00 -0700

    feat: load configuration from evoke.yaml

---
+++ cmd/evoke/config.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+func loadConfig() (map[string]interface{}, error) {
+	configFile, err := os.ReadFile("evoke.yaml")
+	if err != nil {
+		// If the file doesn't exist, return an empty config
+		if os.IsNotExist(err) {
+			return make(map[string]interface{}), nil
+		}
+		return nil, err
+	}
+
+	var config map[string]interface{}
+	err = yaml.Unmarshal(configFile, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
