eb19e8d7d4085fa1dee1463a71bfcc6d5b128f7a
Bitlatte <cobymckinney2025@gmail.com>
2025-07-07 10:43:03 -0700

    test: new benchmarks

--- pkg/content/content_test.go
+++ pkg/content/content_test.go
@@ -82,3 +82,79 @@ func TestProcessMarkdown_WithLayout(t *testing.T) {
 	os.RemoveAll(contentDir)
 	os.RemoveAll(partialsDir)
 }
+
+func BenchmarkProcessHTML(b *testing.B) {
+	// Arrange
+	distDir := "dist"
+	contentDir := "content"
+	partialsDir := "partials"
+	os.RemoveAll(distDir)
+	os.MkdirAll(contentDir, 0755)
+	os.MkdirAll(partialsDir, 0755)
+
+	// Create a dummy content file
+	os.WriteFile("content/index.html", []byte("<h1>Hello</h1>"), 0644)
+	// Create a dummy layout file
+	os.WriteFile("content/_layout.html", []byte("<html><body>{{.Content}}</body></html>"), 0644)
+
+	config := map[string]interface{}{}
+	loadedPartials, _ := partials.LoadPartials()
+	contentProcessor, err := content.New(config, loadedPartials)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	// Act
+	for i := 0; i < b.N; i++ {
+		err := contentProcessor.ProcessHTML("content/index.html")
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+
+	// Clean up
+	os.RemoveAll(distDir)
+	os.RemoveAll(contentDir)
+	os.RemoveAll(partialsDir)
+}
+
+func BenchmarkProcessMarkdown(b *testing.B) {
+	// Arrange
+	distDir := "dist"
+	contentDir := "content"
+	partialsDir := "partials"
+	os.RemoveAll(distDir)
+	os.MkdirAll(contentDir, 0755)
+	os.MkdirAll(partialsDir, 0755)
+
+	// Create a dummy content file
+	os.WriteFile("content/post.md", []byte("# My Post"), 0644)
+	// Create a dummy layout file
+	os.WriteFile("content/_layout.html", []byte("<html><body>{{.Content}}</body></html>"), 0644)
+
+	config := map[string]interface{}{}
+	loadedPartials, _ := partials.LoadPartials()
+	contentProcessor, err := content.New(config, loadedPartials)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	// Act
+	for i := 0; i < b.N; i++ {
+		err := contentProcessor.ProcessMarkdown("content/post.md")
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+
+	// Clean up
+	os.RemoveAll(distDir)
+	os.RemoveAll(contentDir)
+	os.RemoveAll(partialsDir)
+}
